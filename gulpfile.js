7b7efb36a47e99cc213ece7c4a2f87a0d8d23c9d
Miguel Ángel <miguel@miduga.es>
2016-02-07 16:53:39 +0100

    Adding some gulp taks and some improvements.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,24 @@ const gulp = require('gulp')
 const surge = require('gulp-surge')
 const htmlmin = require('gulp-htmlmin')
 const del = require('del')
+const stylus = require('gulp-stylus')
+
+gulp.task('watch', () => {
+  gulp.watch('./index.html', ['minify-html'])
+  gulp.watch('*.styl', ['compily-stylus'])
+})
+
+gulp.task('compily-stylus', () => {
+  gulp.src('*.styl')
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(gulp.dest('./www/'))
+})
 
 gulp.task('minify-html', () => {
   del(['www/index.html']).then(paths => {
-    gulp.src('*.html')
+    gulp.src('index.html')
       .pipe(htmlmin({collapseWhitespace: true}))
       .pipe(gulp.dest('www'))
   })
@@ -15,7 +29,7 @@ gulp.task('minify-html', () => {
 
 gulp.task('deploy', [ 'minify-html' ], () => {
   return surge({
-    project: './www',         // Path to your static build directory
+    project: './www', // Path to your static build directory
     domain: 'miduga.es'  // Your domain or Surge subdomain
   })
 })
