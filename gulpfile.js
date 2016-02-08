956cfb7247128240f1e482db3f2a61b71917554a
Miguel Ángel <miguel@miduga.es>
2016-02-08 11:43:23 +0100

    Prepare articles converter to static blog

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,48 @@ const gulp = require('gulp')
 const surge = require('gulp-surge')
 const htmlmin = require('gulp-htmlmin')
 const stylus = require('gulp-stylus')
-const ejs = require('gulp-ejs')
+const gulpejs = require('gulp-ejs')
+const ejs = require('ejs')
+const markdown = require('gulp-markdown')
+const frontMatter = require('gulp-front-matter')
+const data = require('gulp-data')
 
 gulp.task('create-index', () => {
   gulp
     .src('./_layouts/index.ejs')
-    .pipe(ejs({}, {ext: '.html'}))
+    .pipe(gulpejs({}, {ext: '.html'}))
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./www/'))
+})
+
+gulp.task('create-articles', () => {
+  gulp.src('./_posts/*.md')
+    .pipe(frontMatter({
+      property: 'metadata',
+      remove: true
+    }))
+    .pipe(markdown())
+    .pipe(data((file, cb) => {
+      let layout = __dirname + `/_layouts/${file.metadata.layout}.ejs`
+      let data = file.metadata
+      data.article = String(file.contents)
+      ejs.renderFile(layout, data, (err, result) => {
+        if (err) return cb(err)
+        else cb(null, result)
+      })
+    }))
+    .pipe(gulp.dest('./www/articles/'))
 })
 
 gulp.task('watch', () => {
   gulp.watch('./index.html', ['minify-html'])
   gulp.watch('*.styl', ['compily-stylus'])
+  gulp.watch('./_articles/*.md', ['create-articles'])
 })
 
 gulp.task('compily-stylus', () => {
   gulp.src('*.styl')
-    .pipe(stylus({
-      compress: true
-    }))
+    .pipe(stylus({compress: true}))
     .pipe(gulp.dest('./www/'))
 })
 
